9534c106e5aa29c51d46e5fcbef0d9d24018885e
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-24 10:54:17 -0400

    Initial commit

---
+++ MinValue/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MinValue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(13, 26);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(89, 23);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "Get Min Value";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.Location = new System.Drawing.Point(13, 56);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(120, 342);
+            this.lstValues.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 424);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.btnGet);
+            this.Name = "Form1";
+            this.Text = "Min Value";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.ListBox lstValues;
+    }
+}
+
---
+++ MinValue/Form1.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Created by: Jadon F
+ * Created on: 24-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Find Min Value
+ * This program finds the lowest value in a randomized array
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinValue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int getMinValue(int[] tmpArray)
+        {
+            int tmpMin = 500;
+
+            foreach (int inArray in tmpArray)
+            {
+                if (inArray < tmpMin)
+                {
+                    tmpMin = inArray;
+                }
+            }
+
+            return tmpMin;
+
+
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            //clear list
+            lstValues.Items.Clear();
+
+            //locals
+            const int MAX_ARRAY = 10;
+            const int MAX_NUMBER = 500;
+            int[] arrayOfValues = new int[MAX_ARRAY];
+            int counter, random, minValue;
+            Random rGenerator = new Random();
+
+            //for loop
+            for (counter = 0; counter < MAX_ARRAY; counter++)
+            {
+                random = rGenerator.Next(1, MAX_NUMBER + 1);
+
+                lstValues.Items.Add(random);
+
+                arrayOfValues[counter] = random;
+            }
+
+            minValue = getMinValue(arrayOfValues);
+
+            //show message
+            MessageBox.Show("The min value is " + minValue, "Min Value");
+        }
+    }
+}
+
